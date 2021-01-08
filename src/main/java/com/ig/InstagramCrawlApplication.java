9f0dbfe3598ca0c7a3a6aa76621df89de2cf84fa
= <=>
2021-01-08 14:04:14 +0700

    humnay

--- src/main/java/com/ig/InstagramCrawlApplication.java
+++ src/main/java/com/ig/InstagramCrawlApplication.java
@@ -2,12 +2,19 @@ package com.ig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InstagramCrawlApplication {
+public class InstagramCrawlApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InstagramCrawlApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(InstagramCrawlApplication.class);
 	}
 
 }
